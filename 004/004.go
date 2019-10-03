b23dd7a5f0dbd19dd9d15651af1399ba023f3f03
Alex Nathanail <alex@vectare.co.uk>
2019-10-03 11:46:49 +0100

    Create 004.go

---
+++ 004/004.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func reverse(s string) string {
+  runes := []rune(s)
+  for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+    runes[i], runes[j] = runes[j], runes[i]
+  }
+  return string(runes)
+}
+
+func isPallindrome(n string) bool {
+	return n == reverse(n)
+}
+
+func main() {
+	var longest_pallindrome int
+	for i := 100; i <= 999; i++ {
+		for j := i; j <= 999; j++ {
+			var k = i*j
+			if k > longest_pallindrome && isPallindrome(strconv.Itoa(k)) {
+				longest_pallindrome = k
+			}
+		}
+	}
+	fmt.Println(longest_pallindrome)
+}
