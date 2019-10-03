8982fc8f5401a357ec5503bda245490e20de0866
Alex Nathanail <alex@vectare.co.uk>
2019-10-03 17:31:05 +0100

    Update 004.go

--- 004/004.go
+++ 004/004.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
 )
 
 func reverse(s string) string {
@@ -14,18 +14,25 @@ func reverse(s string) string {
 }
 
 func isPallindrome(n string) bool {
-	return n == reverse(n)
+    return n == reverse(n)
 }
 
 func main() {
-	var longest_pallindrome int
-	for i := 100; i <= 999; i++ {
-		for j := i; j <= 999; j++ {
-			var k = i*j
-			if k > longest_pallindrome && isPallindrome(strconv.Itoa(k)) {
-				longest_pallindrome = k
-			}
-		}
-	}
-	fmt.Println(longest_pallindrome)
+    var t int
+    fmt.Scanf("%d", &t)
+    for z := 0; z < t; z++ {
+        var n int
+        fmt.Scanf("%d", &n)
+        var longest_pallindrome int
+
+        for i := 100; i <= 999; i++ {
+            for j := i; j <= 999; j++ {
+                var k = i*j
+                if k < n && k > longest_pallindrome && isPallindrome(strconv.Itoa(k)) {
+                    longest_pallindrome = k
+                }
+            }
+        }
+        fmt.Println(longest_pallindrome)
+    }
 }
