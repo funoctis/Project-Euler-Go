b57a1898d5f63c94920e54be82ae36a45c876230
amalshaji <amalshajid@gmail.com>
2019-10-05 11:26:27 +0530

    Added constraints to the code

--- 002/002.go
+++ 002/002.go
@@ -1,26 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main() {
-	var t int
-	fmt.Scanf("%d", &t)
-	for i := 0; i < t; i++ {
-		var n int
-		fmt.Scanf("%d", &n)
-
-		a := 1
-		b := 2
-		var c, sum int
-		for a < n {
-			if a%2 == 0 {
-				sum += a
-			}
-			c = a + b
-			a = b
-			b = c
-		}
-
-		fmt.Println(sum)
-	}
+    var t int
+    fmt.Scanf("%d", &t)
+    if t >= 1 && t <= 100000 {
+        for i := 0; i < t; i++ {
+            var n int64
+            fmt.Scanf("%d", &n)
+            if n >= 10 && n < 40000000000000000 {
+                var a, b int64 = 1, 2
+                var c, sum int64
+                for a < n {
+                    if a % 2 == 0 {
+                        sum += a
+                    }
+                    c = a + b
+                    a = b
+                    b = c
+                }
+                fmt.Println(sum)
+            }
+        }
+    }
 }
